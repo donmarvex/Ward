479085a9c407bd07d7ddddb14bec12971b0b4a6d
Alexandra Ighodaro <alexandraighodaro@gmail.com>
2022-09-22 08:33:38 +0100

    Created User Database Table

--- RoyalKiddiesWard.API/Program.cs
+++ RoyalKiddiesWard.API/Program.cs
@@ -1,3 +1,7 @@
+using RoyalKiddiesWard.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using RoyalKiddiesWard.Data;
+
 namespace RoyalKiddiesWard.API
 {
     public class Program
@@ -7,9 +11,13 @@ namespace RoyalKiddiesWard.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+           builder.Services.AddDbContext<RoyalKiddiesWardDbContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("RoyalKiddiesWardConnection")),
+             ServiceLifetime.Scoped);
+         
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
--- RoyalKiddiesWard.API/ViewModels/UserLogin.cs
+++ RoyalKiddiesWard.API/ViewModels/UserLogin.cs
@@ -1,6 +1,21 @@
-﻿namespace RoyalKiddiesWard.API.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace RoyalKiddiesWard.API.ViewModels
 {
     public class UserLogin
     {
+        
+        [Required(ErrorMessage = "Please enter Email Address")]
+        [DataType(DataType.EmailAddress)]
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+
+
+
+        [JsonPropertyName("password")]
+        [Required(ErrorMessage = "Please enter password")]
+        public string Password { get; set; }
+
     }
 }
